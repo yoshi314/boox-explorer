c4ead6b77e257a75ed849e1a7cf1e6970148b59c
yoshi314 <yoshi314@gmail.com>
2014-08-20 17:48:49 +0200

    simple sql script for non destructive db updates

---
+++ updates.sql
@@ -0,0 +1,8 @@
+--2014-08-20
+
+
+-- alter the series view wrt new changes in apps
+
+delete from views where id=7;
+
+INSERT INTO views VALUES(7,7,1,'By Series',2,'SELECT DISTINCT series,max(read_date) as series_read_date,sum(read_count) as series_read_count, count(*) as total_books, (select count(*) from books c where c.series=b.series and read_count = 0) as series_unread, (select count(*) from books c where c.series=b.series and read_count > 0) as series_read FROM books b WHERE series <> '''' group by upper(series) ORDER BY read_date desc, upper(series), series;SELECT DISTINCT file, title, author,read_count, series, series_index FROM books WHERE series = :series ORDER BY series_index','/usr/share/explorer/images/middle/series.png');
